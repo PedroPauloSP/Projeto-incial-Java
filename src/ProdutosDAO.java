d327e8b0ae10fc8ebe59eef840f871fdb2f6a187
Pedro Paulo <pedropsantospereira@gmail.com>
2024-06-24 19:38:54 -0300

    Funcionalidade Salvar adicinado

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,41 +13,70 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 
 
 public class ProdutosDAO {
-    
-    Connection conn;
+   private conectaDAO conecta; 
+   private Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
+    
+      public ProdutosDAO() {
+        this.conecta = new conectaDAO();
+        this.conn = this.conecta.connectDB();
+    }
+    
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        int status;
-        try { 
-            prep = conn.prepareStatement("INSERT INTO produtos VALUES (?,?,?)");
-            
-            prep.setString(1, produto.getNome());
-            prep.setDouble(2, produto.getValor());
-            prep.setString(3, produto.getStatus());
-        }catch (Exception ex){
-             System.out.println("Erro ao conectar: " + ex.getMessage());
-            
         
-        }
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)";
+      try{  
+        prep = this.conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setDouble(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.execute();
+      }catch (Exception e) {
+        System.out.println("Erro ao inserir produto: " + e.getMessage()); 
+      }
+       
+        //conn = new conectaDAO().connectDB();   
+    }
+    
+    public List<ProdutosDTO> listarProdutos(){
         
-        //conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos WHERE nome LIKE ?";
+        
+         try {
+                    
+             PreparedStatement stmt = this.conn.prepareStatement(sql);        
+            
+             ResultSet rs = stmt.executeQuery();
+             
+             List<ProdutosDTO> listarproduto = new ArrayList<>();  
+            while (rs.next()){
+             ProdutosDTO produtos  = new ProdutosDTO();
+               
+                produtos.setId(rs.getInt("id"));
+                produtos.setNome(rs.getString("nome"));
+                produtos.setValor(rs.getInt("valor"));
+                produtos.setStatus(rs.getString("status"));
+               
+           
+                listarproduto.add(produtos);
+        
+            }
+        return listarproduto;
+    }catch (Exception e) {
+        return null; 
         
         
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
+    }  
     
         
 }
